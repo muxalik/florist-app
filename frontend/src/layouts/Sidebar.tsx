e35fbdbfdbca00b5f3fecada4647a72697192e8d
Mihail Kornilov <105970406+muxalik@users.noreply.github.com>
2024-02-21 13:12:56 +0300

    chore sidebar paddings

--- frontend/src/layouts/Sidebar.tsx
+++ frontend/src/layouts/Sidebar.tsx
@@ -23,8 +23,8 @@ const Sidebar = () => {
   return (
     <div className='w-[250px] fixed top-0 left-0 bottom-0 h-screen z-20 bg-background border-r border-r-border'>
       <div className='relative w-full h-full overflow-y-hidden'>
-        <ScrollArea className='rounded-md py-4 px-2 pb-[100px]'>
-          <ul className='flex flex-col gap-2'>
+        <ScrollArea className='rounded-md pb-[100px]'>
+          <ul className='flex flex-col gap-2 py-4 px-2'>
             {sidebarLinks.map((link) => (
               <li key={link.title + link.to}>
                 <Button

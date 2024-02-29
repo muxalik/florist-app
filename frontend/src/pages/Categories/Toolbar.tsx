2a6223d85609adcbe5d658169377fadeec73bb30
Mihail Kornilov <105970406+muxalik@users.noreply.github.com>
2024-02-29 16:32:44 +0300

    feature clear filters

--- frontend/src/pages/Categories/Toolbar.tsx
+++ frontend/src/pages/Categories/Toolbar.tsx
@@ -10,6 +10,7 @@ import { Button } from '@/components/ui/button'
 import downloadFromUrl from '@/utils/downloadFromUrl'
 
 import CategoryFilters from './filters'
+import { defaultCategoryFilters } from '@/constants/categories/filters'
 
 export function CategoryToolbar<TData>({
   table,
@@ -19,32 +20,40 @@ export function CategoryToolbar<TData>({
   const onSearch = useCategories((state) => state.onSearch)
 
   const clearFilters = useCategoryFilters((state) => state.clearFilters)
+  const filters = useCategoryFilters((state) => state.filters)
+
+  const hasFiltersChanged = !_.isEqual(filters, defaultCategoryFilters)
 
   return (
     <div className='flex items-end gap-10 justify-between'>
-      <div className='flex flex-1 flex-wrap gap-x-2 gap-y-3 items-start'>
-        <div className='relative'>
-          <Input
-            placeholder='Поиск...'
-            value={search}
-            onChange={onSearch}
-            className='h-8 w-[150px] lg:w-[250px] pl-8'
-          />
-          <Icons.search className='absolute top-1/2 left-3 -translate-y-1/2 text-gray-500 w-4 h-4' />
-        </div>
+      <div className='flex flex-col flex-1 gap-x-2 gap-y-3 items-start'>
+        <div className='flex gap-2'>
+          <div className='relative'>
+            <Input
+              placeholder='Поиск...'
+              value={search}
+              onChange={onSearch}
+              className='h-8 w-[150px] lg:w-[250px] pl-8'
+            />
+            <Icons.search className='absolute top-1/2 left-3 -translate-y-1/2 text-gray-500 w-4 h-4' />
+          </div>
 
-        <DataTableViewOptions table={table} columnNames={columnNames} />
+          <DataTableViewOptions table={table} columnNames={columnNames} />
+        </div>
 
         <CategoryFilters />
 
-        <Button
-          variant={'link'}
-          onClick={clearFilters}
-          size={'sm'}
-          className='underline'
-        >
-          Сбросить все
-        </Button>
+        {hasFiltersChanged && (
+          <Button
+            variant={'link'}
+            onClick={clearFilters}
+            size={'sm'}
+            className='underline'
+          >
+            <span>Сбросить все</span>
+            <Icons.close className='ml-1 w-4 h-4' />
+          </Button>
+        )}
       </div>
       <div className='flex space-x-2'>
         <Button
